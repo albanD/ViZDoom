7e92533333d8261da95d99ce3da0de23713cdcdb
albanD <alban@robots.ox.ac.uk>
2016-06-15 17:18:02 +0100

    use the proposed size

--- src/lib/ViZDoomController.cpp
+++ src/lib/ViZDoomController.cpp
@@ -838,8 +838,8 @@ namespace vizdoom {
         if (!this->heatMapsBuffer) {
             // Hard coded sizes for now
             this->heatMapsChannels = 5;
-            this->heatMapsHeight = 256;
-            this->heatMapsWidth = 256;
+            this->heatMapsHeight = 120;
+            this->heatMapsWidth = 120;
             // 0: walls
             // 1: player
             // 2: medkits
